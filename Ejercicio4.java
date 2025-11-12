b6cf432d04cec30e786fad57f4868b8d352a93f9
Julio <jmarort@g.educaand.es>
2025-11-12 12:11:34 +0100

    Add files via upload

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,9 +1,9 @@
 /*
  * Nombre:Julio
- * Descripcion:
+ * Descripcion:un array que guarde 4 alumnos y 5 asignaturas,se debe mostrar nota mínima,maxima y media
  * Fecha:11/11/2025
  */
-package MárquezOrtegaJulio3_11;
+package MarquezOrtegaJulio3_12;
 
 import java.util.Scanner;
 
@@ -12,15 +12,14 @@ public class Ejercicio4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner num = new Scanner(System.in);
-		int nota = calificaciones[i][a];
+		
 		final int ALUMNOS = 4;
 		final int ASIGNATURAS = 5;
 		int [] [] calificaciones;
 		calificaciones = new int [ALUMNOS][ASIGNATURAS];
-		int media;
-		int total;
-		int minimo = calificaciones[ALUMNOS][0];
-		int maximo = calificaciones [ALUMNOS][0];
+		double media;
+		
+		
 		
 		for(int i = 0;i<ALUMNOS;i++) {
 			for(int a = 0;a<ASIGNATURAS;a++) { //Metemos los valores en este apartado
@@ -40,11 +39,30 @@ public class Ejercicio4 {
 		
 	}
 		for (int i = 0;i<ALUMNOS;i++) {
+			double total = 0;
+			int minimo = calificaciones[i][0];
+			int maximo = calificaciones [i][0];
 			for (int a = 0;a<ASIGNATURAS;a++) {
-				if (ALUMNOS < minimo) {
-					minimo = 
+				
+				int nota = calificaciones[i][a];
+				 total += nota;
+				if (nota < minimo) {
+					minimo = nota ;
 				}
+				if (nota > maximo) {
+					maximo = nota;
+					
+					
+				}
+				
 			}
+			media =  total/ASIGNATURAS;
+			System.out.println("El alumno " + (i+1) + ":");
+			System.out.println("La nota mínima es " + minimo);
+			System.out.println("Nota maxima " + maximo);
+			System.out.println("Nota media: " + media);
 		}
+		
 	}
+
 }
